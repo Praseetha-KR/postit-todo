da20e430661250b28dae52c66db8b8a23a82074f
Praseetha-KR <voidimagineer@yahoo.com>
2016-08-23 02:11:48 +0530

    Add quit icon

--- app/index.js
+++ app/index.js
@@ -1,39 +1,50 @@
-var menubar = require('menubar');
+// var menubar = require('menubar');
 
-var mb = menubar();
+// var mb = menubar();
 
-mb.on('ready', function ready () {
-  console.log('app is ready');
+// mb.on('ready', function ready () {
+//   console.log('app is ready');
+// });
+
+
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    //if (process.platform !== 'darwin') {
+    app.quit();
+    //}
 });
 
-// const electron = require('electron');
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
 
-// const app = electron.app;
-// const BrowserWindow = electron.BrowserWindow;
-
-// let mainWindow;
-
-// function createWindow () {
-//     mainWindow = new BrowserWindow({width: 800, height: 600});
-
-//     mainWindow.loadURL(`file://${__dirname}/index.html`);
-//     mainWindow.webContents.openDevTools();
-
-//     mainWindow.on('closed', function () {
-//         mainWindow = null;
-//     });
-// }
-
-// app.on('ready', createWindow);
-
-// app.on('window-all-closed', function () {
-//     //if (process.platform !== 'darwin') {
-//     app.quit();
-//     //}
+const {ipcMain} = require('electron');
+// ipcMain.on('asynchronous-message', (event, arg) => {
+//     console.log(arg);
+//     event.sender.send('asynchronous-reply', 'pong');
 // });
 
-// app.on('activate', function () {
-//     if (mainWindow === null) {
-//         createWindow();
-//     }
-// });
+ipcMain.on('quit', (event, arg) => {
+    app.quit();
+});
