45d14555fddd98f20fc3040be5ca856411abd66c
Praseetha-KR <voidimagineer@yahoo.com>
2016-08-25 03:22:41 +0530

    Implement task delete, Fix styling & finalized app

--- app/index.js
+++ app/index.js
@@ -1,50 +1,47 @@
-// var menubar = require('menubar');
-
-// var mb = menubar();
-
-// mb.on('ready', function ready () {
-//   console.log('app is ready');
-// });
-
-
 const electron = require('electron');
+const {ipcMain} = require('electron');
+var menubar = require('menubar');
 
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+var mb = menubar();
 
-let mainWindow;
+mb.on('ready', function ready () {
+    console.log('app is ready');
+});
 
-function createWindow () {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-    mainWindow.webContents.openDevTools();
-
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function () {
-    //if (process.platform !== 'darwin') {
+ipcMain.on('quit', () => {
     app.quit();
-    //}
 });
 
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
 
-const {ipcMain} = require('electron');
-// ipcMain.on('asynchronous-message', (event, arg) => {
-//     console.log(arg);
-//     event.sender.send('asynchronous-reply', 'pong');
+// const BrowserWindow = electron.BrowserWindow;
+
+// let mainWindow;
+
+// function createWindow () {
+//     mainWindow = new BrowserWindow({width: 800, height: 600});
+
+//     mainWindow.loadURL(`file://${__dirname}/index.html`);
+//     mainWindow.webContents.openDevTools();
+
+//     mainWindow.on('closed', function () {
+//         mainWindow = null;
+//     });
+// }
+
+// app.on('ready', createWindow);
+
+// app.on('window-all-closed', function () {
+//     //if (process.platform !== 'darwin') {
+//     app.quit();
+//     //}
 // });
 
-ipcMain.on('quit', (event, arg) => {
-    app.quit();
-});
+// app.on('activate', function () {
+//     if (mainWindow === null) {
+//         createWindow();
+//     }
+// });
+
+
+
